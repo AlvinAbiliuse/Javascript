7197947440b831f74b9146a53028aff5e29a6108
Alvin Abiliuse <alvinabiliuse@gmail.com>
2024-11-05 05:30:13 +0400

    string-operator.js

---
+++ Javascript/2/js/string-operator.js
@@ -0,0 +1,8 @@
+var greeting = "Howdy ";
+var name = "Molly";
+
+var elGreet = document.getElementById("greeting");
+elGreet.textContent = greeting;
+
+var elName = document.getElementById("name");
+elName.textContent = name;
