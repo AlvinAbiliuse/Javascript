a9770b1e3c3e1ef2813d3be906275a5dd9a5af9f
Alvin Abiliuse <alvinabiliuse@gmail.com>
2024-11-12 10:56:16 +0400

    example.js

--- Javascript/3/js/example.js
+++ Javascript/3/js/example.js
@@ -16,8 +16,30 @@
 	specialRate = document.getElementById("specialRate");
 
 	hotelName.textContent = hotel.name;
-	roomRate.textContent = hotel.roomRate;
-	specialRate.textContent = hotel.specialRate();
+	roomRate.textContent = "$" +  hotel.roomRate;
+	specialRate.textContent = "$" + hotel.specialRate();
 
+	var expiryMsg;
+	var today;
+	var elEnds;
+
+	function offerEnds(today) {
+		var weekFromToday, day, date, month, year, dayNames, monthNames;
+		
+		weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+		dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+		monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		day = dayNames[weekFromToday.getDay()];
+		date = weekFromToday.getDate();
+		month = monthNames[weekFromToday.getMonth()];
+		year = weekFromToday.getFullYear();
+		expiryMsg = "Offer Expires next " + day + "<br />(";
+		expiryMsg += date + ' ' + month + " " + year + ")";
+		return expiryMsg;
+	}
+
+	today = new Date();
+	elEnds = document.getElementById("offerEnds");
+	elEnds.innerHTML = offerEnds(today);
 }())
 	
