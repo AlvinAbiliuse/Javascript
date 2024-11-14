c2a4b9593d9ab7146eb8e6674b4e0ae8a1e22b0d
Alvin Abiliuse <alvinabiliuse@gmail.com>
2024-11-14 09:30:43 +0400

    set-attribute.js

--- Javascript/5/js/set-attribute.js
+++ Javascript/5/js/set-attribute.js
@@ -4,3 +4,7 @@ elOne.className = "complete";
 var elLast = document.getElementsByTagName("li")[3];
 elLast.setAttribute("class", "cool");
 
+var elTwo = document.getElementById("two");
+var classTwo = elTwo.className + " hello";
+elTwo.className = classTwo;
+
