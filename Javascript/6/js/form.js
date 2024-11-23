7eb027ed050fccb1966e7dbeb6ff1e091287e582
Alvin Abiliuse <alvinabiliuse@gmail.com>
2024-11-23 09:17:08 +0400

    form.js

--- Javascript/6/js/form.js
+++ Javascript/6/js/form.js
@@ -9,13 +9,26 @@ elTerms = document.getElementById("terms");
 elTermsHint = document.getElementById("termsHint");
 
 function checkSelect() {
-	var elPackage = this.options[this.selectedIndex].value;
- 	if (elPackage != "year") {
-		elPackageHint.innerHTML = "save $xyz if you pay for 1 year!";
-	} else {
+	var elPackage = this.options[this.selectedIndex].id;
+ 	if (elPackage === "year") {
+		
 		elPackageHint.innerHTML = "Wise Choice";
+	} else {
+		
+		elPackageHint.innerHTML = "save some money if you pay for 1 year!";
 	}
 }
 
+function checkTerms(e) {
+	if (!elTerms.checked) {
+		elTermsHint.className = "warning";
+		elTermsHint.innerHTML = "You must agree to the terms to continue!";
+		event.preventDefault();	
+	} else {
+		elTerms.className = "";
+		elTerms.innerHTML = "";
+	}
+}
 
-elForm.addEventListener("change", checkSelect, false);
+elForm.addEventListener("submit", checkTerms, false);
+elSelectPackage.addEventListener("change", checkSelect, false);
